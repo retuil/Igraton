df39692783795759705d945a02ce470b2be4586a
AndreyRomanov <aromanovekb@gmail.com>
2024-04-27 17:40:34 +0500

    карта, поворот персонажа

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
@@ -6,11 +7,13 @@ public class PlayerScript : MonoBehaviour
     private const float moveSpeed = 400;
 
     private Rigidbody2D physic;
+    private GameObject sprite;
 
     private void Start()
     {
         StartCoroutine(FixedUpdateCoroutine());
         physic = GetComponent<Rigidbody2D>();
+        sprite = GameObject.Find("GG");
     }
 
     private IEnumerator FixedUpdateCoroutine()
@@ -19,6 +22,10 @@ public class PlayerScript : MonoBehaviour
         while (true)
         {
             MovementLogic();
+
+            var direction = (Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized;
+            var rotation = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg; //+ BaseAngle;
+            sprite.transform.rotation = Quaternion.Euler(new Vector3(0,0,rotation+90));
             yield return new WaitForFixedUpdate();
         }
     }
@@ -30,7 +37,7 @@ public class PlayerScript : MonoBehaviour
 
         var movement = new Vector3(horizontalInput, verticalInput, 0f) * (moveSpeed * Time.fixedDeltaTime);
         physic.velocity = movement;
-        
+
         // transform.Translate(movement);
     }
 }
